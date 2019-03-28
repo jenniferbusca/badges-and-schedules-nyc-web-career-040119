3a5f771f446bc7867c7cca1c23fe98c6b803ff9b
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-28 15:42:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def _batch_badge_creator(arr)
+def batch_badge_creator(arr)
   new_arr = []
   arr.each do |name | 
     new_arr << "Hello, my name is #{name}."
